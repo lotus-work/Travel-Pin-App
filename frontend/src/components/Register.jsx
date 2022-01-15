296edbc7295bf09ed60aab25c1370bb0bbdc61e2
dev-lotus <lotushotmail111@gmail.com>
2022-01-15 13:48:04 +0530

    commit

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -30,7 +30,7 @@ export default function Register({ setShowRegister }) {
     <div className="registerContainer">
       <div className="logo">
         <Room className="logoIcon" />
-        <span>TravelPin</span>
+        <span>Travel Pin</span>
       </div>
       <form onSubmit={handleSubmit}>
         <input autoFocus placeholder="username" ref={usernameRef} />
